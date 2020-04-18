48e48f9d4bdacb7a84fe72738c35af519122fc53
beedu <b1n4y4k@gmail.com>
2020-04-18 19:07:19 +0530

    perlin noise added

--- game/element.js
+++ game/element.js
@@ -1,11 +1,13 @@
 class Element {
     constructor(x,y,food) {
-        this.x=x;
-        this.y=y;
-        this.dimension=20;
-        this.food=food; //true => food, false => poison
+        this.x = x;
+        this.y = y;
+        this.dimension = 20;
+        this.food = food; //true => food, false => poison
         this.health = loadImage('../resources/health.png');
         this.poison = loadImage('../resources/poison.png');
+        this.xoff = random(100);
+        this.spd = 2.5;
     }
 
     show() {
@@ -20,7 +22,8 @@ class Element {
     }
 
     vibrate() {
-        this.x+=random(-3,3);
-        this.y+=random(-3,3);
+        this.x += map(noise(this.xoff),0,1,-this.spd,this.spd);
+        this.y += map(noise(this.xoff+100),0,1,-this.spd,this.spd);
+        this.xoff += 0.01;
     }
 }
